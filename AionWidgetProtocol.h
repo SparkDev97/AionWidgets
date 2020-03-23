314a92d2e7b2746c62458629c323c66b95ef9908
SparkDev <info@sparkdev.me>
2020-03-23 17:21:04 +0000

    Update AionWidgetProtocol.h

--- AionWidgetProtocol.h
+++ AionWidgetProtocol.h
@@ -1,15 +1,14 @@
 @protocol AionWidgetProtocol <NSObject>
 
 @required
-@property (nonatomic, assign) BOOL isActivePage;
--(void)setupWithFrame:(CGRect) frame;
--(UIView*)contentView;
+@property (nonatomic, assign) BOOL isActivePage; // Set when this widget is on the current Aion page
+-(void)setupWithFrame:(CGRect) frame; // Where you setup the content view
+-(UIView*)contentView; // The view to be added to the aion page
 
 @optional
--(void)didAppear;
--(void)didScrollToView;
--(void)didScrollAwayFromView;
--(void)AionViewDidHide:(BOOL)hidden;
--(void)viewDidDisappear;
--(void)viewDidAppear;
+-(void)didScrollToView; // Did scroll to widget page
+-(void)didScrollAwayFromView; // Did scroll away from widget page
+-(void)AionViewDidHide:(BOOL)hidden; // Aion did hide
+-(void)viewDidDisappear; // Widget page did disappear by scroll or Aion being dismissed
+-(void)viewDidAppear; // Widget page did appear by scroll or Aion being invoked
 @end
